0a7ceb6d390e277ace9277974635d1fff4830509
caiogmello <caiogomedemello@gmail.com>
2024-08-27 16:50:19 -0300

    generating tests

--- main.py
+++ main.py
@@ -1,60 +1,23 @@
 from UserInterface.Console import Console
-from UserInterface.Factory import Factory
-from User.Professor import Professor
 from Library.Library import Library
+from User.User import User
+from User.Professor import Professor
+from UserInterface.Data import getBooks, getUsers
+
 
 if __name__ == "__main__":
-    # console = Console.getConsole()
-    # console.service()
     lib: Library = Library.getLibrary()
-    book1 = {
-        "id": "1",
-        "copyId": "21",
-        "title": "Author1",
-        "publisher": "Publisher",
-        "authors": ["Author1"],
-        "edition": "1",
-        "year": "2021"
-    }
-    book2 = {
-        "id": "31",
-        "copyId": "23",
-        "title": "Author2",
-        "publisher": "Publisher1",
-        "authors": ["Author1", "Author2"],
-        "edition": "3",
-        "year": "2020"
-    }
-    book3 = {
-        "id": "31",
-        "copyId": "24",
-        "title": "Author2",
-        "publisher": "Publisher1",
-        "authors": ["Author1", "Author2"],
-        "edition": "3",
-        "year": "2020"
-    }
 
-    lib.addBookByDict(book1)
-    lib.addBookByDict(book2)
-    lib.removeBook(lib.getBookById("3"))
-    lib.addBookByDict(book3)
-    lib.addBookByDict(book3)
-    lib.removeBook(lib.getBookById(book3["id"]))
+    books = getBooks()
+    users = getUsers()
 
+    for book in books:
+        lib.addBookByDict(book)
 
-    print("Available books")	
-    for l in lib.getAvailableBooks():
-        print(l)
+    lib.reserveBook(users[0], "100")
+    lib.reserveBook(users[1], "100")
+    lib.reserveBook(users[2], "102")
 
-    print("Unavailable books")
-    for l in lib.getUnavailableBooks():
-        print(l)
-
-    id = "31"
-    print(f"Available copies w id {id}")
-    print(lib.getAvailableBookCopies(lib.getBookById(id)))
-
-    print(f"Total copies w id {id}")
-    print(lib.getTotalBookCopies(lib.getBookById(id)))
+    console = Console.getConsole()
+    console.service()
     
