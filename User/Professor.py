fc9266f4b2d2ff55b3362b3fb6f4ec7cfbe1e0d1
IgorPBorja <igorpradoborja@gmail.com>
2024-08-26 23:33:15 -0300

    complete User subclasses

--- User/Professor.py
+++ User/Professor.py
@@ -1,2 +1,17 @@
-class Professor:
-    pass
+from User.User import User
+from Book.Book import Book
+from Observer.Observer import Observer
+from Operation.Loan.ProfessorLoan import ProfessorLoan
+
+class Professor(User, Observer):
+    dupReservationNotificationCnt: int
+
+    def __init__(self, id: str, name: str):
+        super().__init__(id, name)
+        self.maxLoanTimeDays = 7
+        self.maxOpenLoanOperations = None
+        self.loanOperation = ProfessorLoan()
+        self.dupReservationNotificationCnt = 0
+
+    def update(self, observedBook: Book):
+        self.dupReservationNotificationCnt += 1
