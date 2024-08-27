2f8ecb51a28eeda2bcd2ab2c87318c8cb633db67
caiogmello <caiogomedemello@gmail.com>
2024-08-27 09:59:46 -0300

    factory methods for book

--- UserInterface/Factory.py
+++ UserInterface/Factory.py
@@ -1,2 +1,23 @@
+from Book.Book import Book
+from datetime import datetime
+
 class Factory:
-    pass
+
+    @staticmethod
+    def createBook(bookId: int, exampleId: int,
+                   title: str, publisher: str, authors: list[str],
+                   edition: str, year: datetime) -> Book:
+        return Book(bookId, exampleId, title, publisher, authors, edition, year)
+    
+    @staticmethod
+    def createBookFromDict(bookDict: dict) -> Book:
+        return Factory.createBook(
+            bookDict["bookId"],
+            bookDict["exampleId"],
+            bookDict["title"],
+            bookDict["publisher"],
+            bookDict["authors"],
+            bookDict["edition"],
+            bookDict["year"]
+        )
+    
