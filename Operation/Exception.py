eb49a72e0ddf6b25d3cb969eea961ef9dbe109c4
caiogmello <caiogomedemello@gmail.com>
2024-08-27 11:54:41 -0300

    comment on User import to avoid circular import

--- Operation/Exception.py
+++ Operation/Exception.py
@@ -1,17 +1,17 @@
 from Operation.Operation import Operation
-from User.User import User
+# from User.User import User
 from Book.Book import Book
 
 class OperationException(Exception):
     def __init__(
         self,
         operationType: Operation,
-        user: User,
+        user: 'User',
         item: Book,
         reason: str,
     ):
         self.message = (
-            f"Operação '{operationType}' não pôde ser realizada para o usuário '{user.name}'"
-            f" e livro '{item.id}' pelo seguinte motivo: '{reason}'"
+            f"Operação de '{operationType}' não pôde ser realizada para o usuário '{user.name}'"
+            f" e livro '{item.getId()}' pelo seguinte motivo: '{reason}'"
         )
         super().__init__(self.message)
