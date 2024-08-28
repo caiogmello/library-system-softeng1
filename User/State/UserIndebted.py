1ec2007eedb36cf79051fe08ae92208cd23d7d67
caiogmello <caiogomedemello@gmail.com>
2024-08-28 09:00:36 -0300

    making a directory for the UserState classes

---
+++ User/State/UserIndebted.py
@@ -0,0 +1,11 @@
+from User.State.UserState import UserState
+from Operation.Exception import OperationException
+
+class UserIndebted(UserState):
+    def loanBook(self, user: 'User', bookId: int) -> None:
+        raise OperationException(
+            operationType=user.loanOperation,
+            user=user,
+            bookId=bookId,
+            reason="Usuário está em débito",
+        )
